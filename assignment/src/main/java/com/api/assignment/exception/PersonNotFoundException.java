0c4ce49d12a819ddd4eb9a18ac24df0b7293cc9a
Shekhar Keshri <shekhark@digitalbridge.com.au>
2020-02-24 16:56:32 +0530

    commit

--- assignment/src/main/java/com/api/assignment/exception/PersonNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.api.assignment.exception;
-
-public class PersonNotFoundException extends RuntimeException{
-
-	public PersonNotFoundException(Long id) {
-		super("Person id not found: " +id);
-	}
-	
-}
--- assignment/src/main/java/com/api/assignment/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.assignment.repository;
-
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-import com.api.assignment.node.Person;
-
-public interface PersonRepository extends Neo4jRepository<Person,Long> {
-
-}
